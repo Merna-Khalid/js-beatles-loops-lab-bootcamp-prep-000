1c7d6a01226052873ac5ec5fca50c93b1951267e
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-15 13:42:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,24 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = []
   for(var i = 0; i < musicians.length; i++) {
-    
+    arr.push( musicians[i] + " plays " + instruments[i])
   }
+}
+
+function johnLennonFacts(facts) {
+  var counter = facts.length
+  while(counter--)
+  {
+    facts[counter] += "!!!"
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n) {
+  var arr = []
+  do {
+    arr.push("I love the Beatles!")
+    n++
+  } while( n < 15)
+  return arr
 }
